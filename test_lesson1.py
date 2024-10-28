5e80ae1f0ad2a321b06dc47bea1762ec7b14c105
ntairova <38103230+ntairova@users.noreply.github.com>
2024-10-28 21:01:11 +0300

    Update test_lesson1.py

--- test_lesson1.py
+++ test_lesson1.py
@@ -1,6 +1,5 @@
 numbers = [1, 6, 3, 44, 2, 13]
 for i in range(len(numbers)-1):
-    print(i)
     for j in range(len(numbers)-1):
         if numbers[j] > numbers[j+1]:
             numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
